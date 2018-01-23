5002d56c2aedf69fded8929800c8180fa2ccdb2a
Miguel Mejias <miki.mejias85@gmail.com>
2018-01-23 14:56:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,5 @@ function accessElementInArray(chocolateBars, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
-    return chocolateBars.slice(1);
+    return chocolateBars.shift();
 }
