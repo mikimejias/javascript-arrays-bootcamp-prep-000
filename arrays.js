11faf7250b5b62bb61c9e9ccca03dac84320d258
Miguel Mejias <miki.mejias85@gmail.com>
2018-01-23 14:53:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(chocolateBars, element) {
+    var newArray = [element, ...chocolateBars];
+    return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element) {
+    chocolateBars.unshift(element);
+    return chocolateBars;
+}
+
+function addElementToEndOfArray(chocolateBars, element) {
+    var newArray = [...chocolateBars, element];
+    return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, element) {
+    chocolateBars.push(element);
+    return chocolateBars;
+}
+
+function accessElementInArray(chocolateBars, index) {
+    return chocolateBars[index];
+}
